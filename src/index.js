ba30c7009b0f8dd848acbfefe217d5955512a404
Tiago Evangelista Pinto <tiagoevanp@hotmail.com>
2021-06-14 02:01:55 -0300

    start project with all configs and first test running

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import Phaser from 'phaser';
+
+import Game from './scenes/game';
+
+const config = {
+	type: Phaser.AUTO,
+	width: 800,
+	height: 600,
+	physics: {
+		default: 'arcade',
+		arcade: {
+			gravity: { y: 200 },
+		},
+	},
+	scene: Game,
+};
+
+new Phaser.Game(config);
---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+const pathModule = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const paths = {
+	root: '.',
+	dist: './dist',
+	src: './src',
+	get(key) {
+		return pathModule.resolve(__dirname, this[key]);
+	},
+};
+
+module.exports = (env) => ({
+	context: paths.get('root'),
+
+	devtool: 'source-map',
+	mode: env.BUILD_MODE,
+
+	entry: `${ paths.get('src') }/index.js`,
+
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: `${ paths.get('src') }/index.html`,
+		}),
+	],
+
+	devServer: {
+		historyApiFallback: true,
+		contentBase: paths.get('dist'),
+		watchContentBase: true,
+		inline: true,
+		host: '0.0.0.0',
+		port: 3000,
+	},
+
+	output: {
+		filename: 'bundle.js',
+		path: paths.get('dist'),
+		publicPath: env.ASSET_PATH,
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: [
+					/node_modules/,
+				],
+				loader: 'babel-loader',
+				options: {
+					presets: ['@babel/preset-env'],
+				},
+			},
+			{
+				test: /\.mp3$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[name].[ext]',
+							outputPath: 'resources/',
+						},
+					},
+				],
+			},
+			{
+				test: /\.png$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[name].[ext]',
+							outputPath: 'resources/',
+						},
+					},
+					{
+						loader: 'image-webpack-loader',
+					},
+				],
+			},
+		],
+	},
+});
